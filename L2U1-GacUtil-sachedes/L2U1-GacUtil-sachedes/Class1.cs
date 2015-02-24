c1a1a4ed7468bb2e95e7685e1b5b0701f8557685
des <sachedes@students.zhaw.ch>
2015-02-24 20:54:52 +0100

    initial commit

---
+++ L2U1-GacUtil-sachedes/L2U1-GacUtil-sachedes/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L2U1_GacUtil_sachedes
+{
+    public class Class1
+    {
+        public void defaultmethod()
+        {
+
+        }
+    }
+}
---
+++ W1-H1-Aufgabe1/W1-H1-Aufgabe1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1_H1_Aufgabe1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name;
+            Console.WriteLine("Please type in your name:");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hallo " + name);
+            Console.ReadLine();
+        }
+    }
+}
