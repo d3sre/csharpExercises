b949ea7993a801e86d9e76f943dec7b2c844af95
des <d.sacher@gmail.com>
2015-03-22 16:53:35 +0100

    unterrichtsaufgaben

---
+++ L5M3/L5M3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5M3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DirectoryInfo di = new DirectoryInfo(@"C:\Users\des\Documents\git\csharpExercises");
+            var selectedFiles = from FileInfo f in di.GetFiles("*.*",SearchOption.AllDirectories)
+                                where f.Name.StartsWith("A")
+                                select new {MyName=f.Name, MyCreationTime = f.CreationTime,MyLength=f.Length};
+
+            foreach (var fileInfo in selectedFiles)
+            {
+                Console.WriteLine("Creation Time: {0}, Name: {1}, Length: {2}",fileInfo.MyCreationTime, fileInfo.MyName, fileInfo.MyLength );
+                
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
